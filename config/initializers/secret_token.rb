ac5c5cc9ed1a67b3019ee59e39e87889076a5442
Kumar Abinash <abinash.b2@gmail.com>
2015-02-27 09:41:36 +0530

    USMLE Aid Prject. The whole project('map' branch) will completely focus on usmleaid Maps and markers.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Usmleaid::Application.config.secret_key_base = '93e44d6a8a917df2e66aa6887eee58731e277bed73dde029a817505b1e3de61997b94f17987546f4cc442cf32aec3d0f12275bf017aaa59638ced60e38bff229'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Usmleaid::Application.config.session_store :cookie_store, key: '_usmleaid_session'
